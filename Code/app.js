6ce36e323571ec870de91b69d5405fd18f4a6a5c
amaneru <amaneru@163.com>
2020-07-30 18:00:47 +0800

    调整 table 创建顺序

--- Code/app.js
+++ Code/app.js
@@ -5,8 +5,8 @@ module.exports = app => {
   app.beforeStart(async function() {
     // 同步模型
     const syncModels = [
-      'ad_group', 'management', 'role', 'tenant', 'user', 'user_gourp_mapping',
-      'tenant_group_mapping', 'assign', 'expiry',
+      'ad_group', 'role', 'tenant', 'user', 'user_gourp_mapping',
+      'tenant_group_mapping', 'assign', 'expiry', 'management',
     ];
     for (const syncModel of syncModels) {
       await app.model.models[syncModel].sync();
