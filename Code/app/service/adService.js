8de5a879d50fe3d22e3ebbb8038fcac736a345f4
Tom <tomqi@apjcorp.com>
2020-07-30 15:36:21 +0800

    user tenants update

--- Code/app/service/adService.js
+++ Code/app/service/adService.js
@@ -2,38 +2,40 @@
 
 const axios = require('axios');
 
-const url = 'http://10.231.131.123:3010/';
-// const url = 'http://127.0.0.1:7002/';
+// const url = 'http://10.231.131.123:3010/';
+const url = 'http://127.0.0.1:7002/';
 const jwt = require('jsonwebtoken');
 module.exports = app => {
 
   return class extends app.Service {
     async authenticate(username, password) {
-      const auth = await axios.post(url + 'adService/authenticate', { username, password }
+      const axiosResult = await axios.post(url + 'adService/authenticate', { username, password }
       ).then(function(response) {
         return new Promise(resolve => {
           const data = response.data.data;
           const result = {};
           if (data.auth) {
             // result.user = data.user;
-            const username = data.user.sAMAccountName;
+            const user = data.user;
+            const groups = data.groups;
+            result.user = user;
+            result.groups = groups;
             const token = jwt.sign({
-              username,
+              username: user.sAMAccountName,
             }, '1234567abc', { expiresIn: '10m' });
-            console.log('token', token);
             result.token = token;
             resolve(result);
           } else {
-            resolve(null);
+            resolve(false);
           }
         });
       }).catch(function(error) {
         console.log(error);
         return new Promise(resolve => {
-          resolve(false);
+          resolve(null);
         });
       });
-      return auth;
+      return axiosResult;
     }
   };
 };
