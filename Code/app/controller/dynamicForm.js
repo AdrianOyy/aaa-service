b42637bdde0f37e7b33a06eb474190e434317d4a
amaneru <amaneru@163.com>
2020-08-21 10:55:00 +0800

    测试方法， 还差子表渲染表

--- Code/app/controller/dynamicForm.js
+++ Code/app/controller/dynamicForm.js
@@ -22,23 +22,60 @@ module.exports = app => {
       const body = {
         formKey: 'VMALLOCATION',
         deploymentId: '67501',
-        list: '[{"id":"Tnenant","name":"${tenant.code | name}#list","type":"string","readable":true,"writable":true,"required":true,"formProperty":null,"childTable":null},{"id":"Text","name":"text#text","type":"string","readable":true,"writable":true,"required":false,"formProperty":null,"childTable":null},{"id":"vmList","name":null,"type":"enum","readable":true,"writable":true,"required":false,"formProperty":null,"childTable":{"CPU":"string","Type":"${type.id | name}#list"}}]',
+        list: '[{"id":"Tenant","name":"${tenant.code | name}#list","type":"string","readable":true,"writable":true,"required":true,"formProperty":null,"childTable":null},{"id":"Text","name":"text#text","type":"string","readable":true,"writable":true,"required":false,"formProperty":null,"childTable":null},{"id":"vmList","name":null,"type":"enum","readable":true,"writable":true,"required":false,"formProperty":null,"childTable":{"CPU":"string","Type":"${type.id | name}#list"}}]',
       };
+      const { formKey, deploymentId, list } = body;
       const basicFormFieldList = [];
       const childTableList = [];
-      JSON.parse(body.list).forEach(el => {
+      JSON.parse(list).forEach(el => {
         if (el.type === 'enum') {
           childTableList.push(el);
         } else {
           basicFormFieldList.push(el);
         }
       });
-      const createBasicFormSQL = await ctx.service.dynamicForm.getBasicSQL(body.formKey, basicFormFieldList);
-      const createChildTableSQLList = await ctx.service.dynamicForm.getChildTableSQLList(childTableList);
-      [ createBasicFormSQL, ...createChildTableSQLList ].forEach(SQL => {
-        app.model.query(SQL);
+
+      // 记录表
+      // 父表
+      const basicDynamicForm = await ctx.model.models.dynamicForm.create({
+        deploymentId,
+        formKey,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       });
-      ctx.success();
+      // 子表
+      const childFormData = [];
+      childTableList.forEach(el => {
+        const model = {
+          parentId: basicDynamicForm.id,
+          formKey: el.id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+        childFormData.push(model);
+      });
+      const childDynamicFormList = await ctx.model.models.dynamicForm.bulkCreate(childFormData);
+
+      // 渲染表
+      // 父表
+      const basicDynamicFormDetailData = await ctx.service.dynamicForm.getBasicDynamicFormDetailData(basicDynamicForm.id, basicFormFieldList);
+
+      await ctx.model.models.dynamicFormDetail.bulkCreate(basicDynamicFormDetailData);
+
+
+      // 创建数据表
+      const createBasicFormSQL = await ctx.service.dynamicForm.getBasicSQL(formKey, basicFormFieldList);
+      const createChildTableSQLList = await ctx.service.dynamicForm.getChildTableSQLList(formKey, childTableList);
+      const SQLList = [ createBasicFormSQL, ...createChildTableSQLList ];
+      try {
+        for (let i = 0; i < SQLList.length; i++) {
+          await app.model.query(SQLList[i]);
+        }
+        ctx.success();
+      } catch (error) {
+        ctx.error(error);
+      }
+
     }
   };
 };
--- Code/app/service/dynamicForm.js
+++ Code/app/service/dynamicForm.js
@@ -19,13 +19,13 @@ module.exports = app => {
         }
         fieldList += `\`${el.id}\` ${fieldType},`;
       });
-      fieldList = fieldList.toString().slice(0, -1);
+      fieldList += 'UNIQUE(pid)';
       const basicFormSQL = `CREATE TABLE ${formKey} (${fieldList})`;
       return basicFormSQL;
     }
 
-    async getChildTableSQLList(list) {
-      const childeTableSQLList = [];
+    async getChildTableSQLList(parentFormKey, list) {
+      const childTableSQLList = [];
       list.forEach(el => {
         let fieldList = '`id` int unsigned not null auto_increment primary key, `pid` int,';
         for (const key in el.childTable) {
@@ -43,19 +43,71 @@ module.exports = app => {
           }
           fieldList += `\`${key}\` ${fieldType},`;
         }
-        fieldList = fieldList.toString().slice(0, -1);
-        const childeTableSQL = `CREATE TABLE ${el.id} (${fieldList})`;
-        childeTableSQLList.push(childeTableSQL);
+        fieldList += `FOREIGN KEY(pid) REFERENCES ${parentFormKey}(pid) on delete cascade on update cascade`;
+        const childTableSQL = `CREATE TABLE ${el.id} (${fieldList})`;
+        childTableSQLList.push(childTableSQL);
       });
-      return childeTableSQLList;
+      return childTableSQLList;
     }
 
-    async getSQL(SQLList) {
-      let res = '';
-      SQLList.forEach(el => {
-        res += el + ';';
+    async getBasicDynamicFormDetailData(dynamicFormId, list) {
+      const dataList = [];
+      list.forEach(el => {
+        const fieldName = el.id;
+        const fieldType = el.type;
+        let inputType = 'text';
+        let foreignTable = null;
+        let foreignKey = null;
+        let foreignDisplayKey = null;
+        let showOnRequest = true;
+
+        if (el.name) {
+          if (el.name[el.name.length - 1] === '!') {
+            showOnRequest = false;
+          }
+          const paramsList = el.name.split('#');
+          if (paramsList.length > 2) return false;
+          if (paramsList.length === 2) {
+            let inputParams = paramsList[1].trim();
+            if (inputParams[inputParams.length - 1] === '!') {
+              showOnRequest = false;
+              inputParams = inputParams.slice(0, -1);
+            }
+            inputType = inputParams.trim();
+          }
+          let foreign = paramsList[0].trim();
+          if (foreign[0] === '$' && foreign[1] === '{' && foreign[foreign.length - 1] === '}') {
+            foreign = foreign.slice(2, -1);
+            const foreignList = foreign.split('|');
+            foreignTable = foreignList[0].split('.')[0].trim();
+            foreignKey = foreignList[0].split('.')[1].trim();
+            if (foreignList.length > 1) {
+              foreignDisplayKey = foreignList[1].trim();
+            } else {
+              foreignDisplayKey = foreignKey;
+            }
+          }
+        }
+
+        const model = {
+          dynamicFormId,
+          fieldName,
+          fieldType,
+          inputType,
+          foreignTable,
+          foreignKey,
+          foreignDisplayKey,
+          showOnRequest,
+        };
+        dataList.push(model);
       });
-      return res;
+
+      return dataList;
     }
+
+    async getChildDynamicFormDetailData(list) {
+
+    }
+
   };
 };
