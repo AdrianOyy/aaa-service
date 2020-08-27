dd9f0c256037f0d0db0b65311cee292987bf3998
Tom <tomqi@apjcorp.com>
2020-08-27 15:50:56 +0800

    add vm_platform_type, vm_platform add type_id

---
+++ Code/app/model/fixtures/vm_platform_type.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = [
+  {
+    model: 'vm_platform_type',
+    data: {
+      name: 'Hardware',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+  {
+    model: 'vm_platform_type',
+    data: {
+      name: 'VMware',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+  {
+    model: 'vm_platform_type',
+    data: {
+      name: 'HCI',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+  {
+    model: 'vm_platform_type',
+    data: {
+      name: 'KVM',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+];
