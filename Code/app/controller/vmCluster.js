a13fe4291fa3ce98b32c6585c36915b7afc48f9d
Tom <tomqi@apjcorp.com>
2020-09-03 17:34:50 +0800

    defineVMType ，updateResource

--- Code/app/controller/vmCluster.js
+++ Code/app/controller/vmCluster.js
@@ -31,5 +31,71 @@ module.exports = app => {
         ctx.error();
       }
     }
+
+    async updateResources() {
+      const { ctx } = this;
+      const {
+        vm_clusters,
+        vm_masters,
+      } = ctx.request.body;
+      // if (!serialNumber) ctx.error();
+      console.log(vm_clusters, vm_masters);
+      try {
+        for (const _ of vm_clusters) {
+          console.log(_.VMClusterName);
+          let vm_cluster = await ctx.model.models.vm_cluster.findOne({
+            where: {
+              VMClusterName: _.VMClusterName,
+            },
+          });
+          console.log(vm_cluster);
+          if (!vm_cluster) {
+            vm_cluster = await ctx.model.models.vm_cluster.create(
+              Object.assign(_, {
+                createdAt: new Date(),
+                updatedAt: new Date(),
+              })
+            );
+          } else {
+            vm_cluster.update(
+              Object.assign(_, {
+                updatedAt: new Date(),
+              })
+            );
+          }
+        }
+        for (const _ of vm_masters) {
+          const vm_cluster = await ctx.model.models.vm_cluster.findOne({
+            raw: true,
+            attributes: [ 'id' ],
+            where: {
+              VMClusterName: _.VMClusterName,
+            },
+          });
+          _.VMClusterId = vm_cluster.id;
+          let vm_master = await ctx.model.models.vm_master.findOne({
+            where: {
+              VMCMasterName: _.VMCMasterName,
+            },
+          });
+          if (!vm_master) {
+            vm_master = await ctx.model.models.vm_master.create(Object.assign(_, {
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            }));
+          } else {
+            vm_master.update(
+              Object.assign(_, {
+                updatedAt: new Date(),
+              })
+            );
+          }
+        }
+        ctx.success(true);
+      } catch (error) {
+        console.log(error);
+        throw { status: 500, message: 'service busy' };
+      }
+    }
   };
 };
