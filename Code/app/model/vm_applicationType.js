2987895d4f932cf1a67131068da794194e082008
amaneru <amaneru@163.com>
2020-09-08 16:03:36 +0800

    hostname update

--- Code/app/model/vm_applicationType.js
+++ Code/app/model/vm_applicationType.js
@@ -6,6 +6,7 @@ module.exports = app => {
   const vm_applicationType = app.model.define('vm_applicationType', {
     id: { type: INTEGER, autoIncrement: true, primaryKey: true, cnName: 'ID' },
     name: { type: STRING(256), allowNull: true, cnName: 'name', comment: 'name' },
+    code: { type: STRING(256), allowNull: true, cnName: 'code', comment: 'code' },
     createdAt: { type: DATE, allowNull: true, cnName: '创建时间', comment: '创建时间' },
     updatedAt: { type: DATE, allowNull: true, cnName: '更新时间', comment: '更新时间' },
   }, {
