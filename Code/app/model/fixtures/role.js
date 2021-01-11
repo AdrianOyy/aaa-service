e85f598a71e01bb7e967ff7e8da4a2a7181c5967
amaneru <amaneru@163.com>
2021-01-11 15:31:00 +0800

    补文件

---
+++ Code/app/model/fixtures/role.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = [
+  {
+    model: 'role',
+    data: {
+      right: 'Read Only',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+  {
+    model: 'role',
+    data: {
+      right: 'Read & Write',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+];
