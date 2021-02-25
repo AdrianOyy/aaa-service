f0a77b8f2cd71405e857bc9bfb72b173bbeafe0b
315121903@qq.com <315121903@qq.com>
2021-02-25 13:51:03 +0800

    model update

--- Code/app/model/resource_request_history.js
+++ Code/app/model/resource_request_history.js
@@ -1,15 +1,14 @@
 'use strict';
 
 module.exports = app => {
-  const { INTEGER, BOOLEAN, DATE, STRING } = app.Sequelize;
+  const { INTEGER, DATE, STRING } = app.Sequelize;
 
   const resource_request_history = app.model.define('resource_request_history', {
     id: { type: INTEGER, autoIncrement: true, primaryKey: true, cnName: 'ID' },
-    workflowId: { type: STRING, allowNull: false, cnName: '流程ID', comment: '流程ID'},
+    workflowId: { type: STRING, allowNull: false, cnName: '流程ID', comment: '流程ID' },
     tenantQuotaMappingId: { type: INTEGER, allowNull: false, cnName: '项目资源年份数量限制Id', comment: '项目资源年份数量限制Id' },
-    success: { type: BOOLEAN, allowNull: false, cnName: '申请成功', comment: '申请成功' },
+    status: { type: STRING, allowNull: false, cnName: '状态', comment: '状态' },
     requestNum: { type: INTEGER, allowNull: false, cnName: '数量', comment: '数量' },
-    successNum: { type: INTEGER, allowNull: false, cnName: '成功数量', comment: '成功数量' },
     createdAt: { type: DATE, allowNull: true, cnName: '创建时间', comment: '创建时间' },
     updatedAt: { type: DATE, allowNull: true, cnName: '更新时间', comment: '更新时间' },
   }, {
